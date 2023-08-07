e7e6840efe947281565e9a5efcb842fd67d68348
Ahmed Adel <70106396+ahmed23-m@users.noreply.github.com>
2023-08-07 10:11:40 +0300

    FireBase Changed

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,141 +1,89 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDjan3X86EnK2UCibt6SO39yo66MDABK0A",
-    authDomain: "noro-14113.firebaseapp.com",
-    databaseURL: "https://noro-14113-default-rtdb.firebaseio.com",
-    projectId: "noro-14113",
-    storageBucket: "noro-14113.appspot.com",
-    messagingSenderId: "761578432013",
-    appId: "1:761578432013:web:175bc5a9ef337580a75744",
-    measurementId: "G-WPM8CBFMJJ",
+    apiKey: "AIzaSyBTWBNR14dECgbNoxd5EZ2Z3BoIbk4_Q1k",
+    authDomain: "aicontrol-3b183.firebaseapp.com",
+    databaseURL: "https://aicontrol-3b183-default-rtdb.firebaseio.com",
+    projectId: "aicontrol-3b183",
+    storageBucket: "aicontrol-3b183.appspot.com",
+    messagingSenderId: "393464001551",
+    appId: "1:393464001551:web:7b878a39f9e0726c9a9f7a",
+    measurementId: "G-28MMZRP17S"
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 var Signals;
 var details = document.querySelector(".Details p");
 
-import {
-    getDatabase,
-    ref,
-    child,
-    onValue,
-} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+import { getDatabase, ref, child, onValue } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 const database = getDatabase();
 
 onValue(child(ref(database), "Signals"), (snapshot) => {
     Signals = snapshot.val();
-    console.log(Signals);
+    // console.log(Signals);
 });
 onValue(child(ref(database), "The Ratio"), (snapshot) => {
     details.textContent = snapshot.val();
 });
 
-const ctx = document.getElementById("myChart");
-const chart_cont = document.querySelector('.Chart');
+const ctx = document.getElementById('myChart');
 
-// var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: [0],
-//         datasets: [
-//             {
-//                 label: 'Signals speed(HZ)',
-//                 data: [0],
-//                 borderWidth: 1,
-//                 pointRadius: 0
-//             },
-//         ]
-//     },
-//     options: {
-//         scales: {
-//             x: {
-//                 display: true,
-//                 title: {
-//                     display: true,
-//                     text: 'Time(seconds)'
-//                 }
-//             },
-//             y: {
-//                 display: true,
-//                 title: {
-//                     display: true,
-//                     text: 'Signals speed(HZ)'
-//                 }
-//             }
-//         }
-//     }
-// });
-// window.setInterval(mycallback, 1000);
-// var i =0;
-// function mycallback() {
-//     i++;
-//     myChart.data.labels.push(i);
-//     myChart.data.datasets[0].data.push(Signals);
-//     if(myChart.data.labels.length>20)
-//     {
-//         myChart.data.labels.splice(0,4);
-//         myChart.data.datasets[0].data.splice(0,4);
-//     }
-//     myChart.update();
-// }
-var trace1 = {
-    x: [0],
-    y: [0],
-    mode: "lines",
-};
-var data = [trace1];
-var layout = {
-    xaxis: {
-        autotick: false,
-        ticks: "inside",
-        tick0: 0,
-        dtick: 1,
-        ticklen: 4,
-        tickwidth: 2,
-        zeroline: true,
-        autorange: true,
-        title: {
-            text: "Time(seconds)",
-            standoff: 20
-        },
-        color: '#ffffffa3'
+let ch = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [0],
+        datasets: [
+            {
+                label: 'Signals speed(HZ)',
+                data: [0],
+                borderWidth: 1,
+                pointRadius: 0,
+                borderColor: '#2196f3',
+                pointBorderColor: "#2196f3",
+                pointHoverBackgroundColor: "#2196f3",
+                pointHoverBorderColor: "#2196f3",
+                fill: false,
+                lineTension: 0
+            }
+        ]
     },
-    yaxis: {
-        title: {
-            text: "Signals speed(HZ)",
-            standoff: 20
-        },
-        color: '#ffffffa3',
-        // lineheight: 30
-    },
-    margin: {
-        l: 70,
-        r: 30,
-        b: 70,
-        t: 50,
-        pad: 10
-    },
-    paper_bgcolor: '#120a1896',
-    plot_bgcolor: '#120a1896',
-    width: chart_cont.width,
-    height: chart_cont.height,
-};
+    options: {
+        scales: {
+            x: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Time(seconds)'
+                }
+            },
+            y: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Signals speed(HZ)'
+                }
+            }
+        }
+    }
+});
+
 window.setInterval(mycallback, 1000);
 var i = 0;
 function mycallback() {
     i++;
-    trace1.x.push(i);
-    trace1.y.push(Signals);
-    if(trace1.x.length>20)
-    {
-        trace1.x.splice(0,4);
-        // trace1.y.splice(0,4);
+    ch.data.labels.push(i);
+    ch.data.datasets[0].data.push(Signals);
+    if (ch.data.labels.length > 20) {
+        ch.data.labels.splice(0, 4);
+        ch.data.datasets[0].data.splice(0, 4);
     }
-    Plotly.newPlot("myChart", data, layout);
+    ch.update();
 }
